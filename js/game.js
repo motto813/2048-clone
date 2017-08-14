1f7560b0cb355b0d1128188b68e46ef872242ce6
Alan English & Paul Hammerschmidt <pair+alanenglish.motto813@devbootcamp.com>
2017-08-14 11:36:16 -0500

    tile constructor function initializes row and column coordinates based on index and starts with value of 2. increase value function will increase value by *2

---
+++ js/game.js
@@ -0,0 +1,16 @@
+var Game = function() {
+  this.board = new Board();
+  this.addTile();
+  this.addTile();
+}
+
+
+// Possible functions
+// game over?
+
+// you can't move that way!
+
+// move that way!
+
+// board: add a tile
+
